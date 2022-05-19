50404bd30667bf09467f62a162c7435e81317fe4
wakywayne <wakywayne80@gmail.com>
2022-05-19 14:29:29 -0400

    trying to fix deployment

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,7 +8,7 @@ export default function about() {
 
             <div className="bg-white shadow-md rounded-lg px-10 py-6 mt-6">
                 <h3 className=' text-2xl mb-5'>
-                    Waky Wayne&apos;s Blog
+                    WakyWayne&apos;s Blog
                 </h3>
                 <p className='mb-3'>
                     This is a blog created in next.js and markdown.
--- pages/api/search.js
+++ pages/api/search.js
@@ -1,43 +1,43 @@
 
-import fs from 'fs'
-import path from 'path'
-import matter from 'gray-matter'
+// import fs from 'fs'
+// import path from 'path'
+// import matter from 'gray-matter'
 
-export default function search(req, res) {
-  let posts
+// export default function search(req, res) {
+//   let posts
 
-  if (process.env.NODE_ENV === 'production') {
-    // Fetch from cache
-    posts = require('../../cache/data').posts
-  } else {
-    const files = fs.readdirSync(path.join('posts'))
+//   if (process.env.NODE_ENV === 'production') {
+//     // Fetch from cache
+//     posts = require('../../cache/data').posts
+//   } else {
+//     const files = fs.readdirSync(path.join('posts'))
 
-    posts = files.map((filename) => {
-      const slug = filename.replace('.md', '')
+//     posts = files.map((filename) => {
+//       const slug = filename.replace('.md', '')
 
-      const markdownWithMeta = fs.readFileSync(
-        path.join('posts', filename),
-        'utf-8'
-      )
+//       const markdownWithMeta = fs.readFileSync(
+//         path.join('posts', filename),
+//         'utf-8'
+//       )
 
-      const { data: frontmatter } = matter(markdownWithMeta)
+//       const { data: frontmatter } = matter(markdownWithMeta)
 
-      return {
-        slug,
-        frontmatter,
-      }
-    })
-  }
+//       return {
+//         slug,
+//         frontmatter,
+//       }
+//     })
+//   }
 
-  const results = posts.filter(
-    ({ frontmatter: { title, excerpt, category } }) =>
-      title.toLowerCase().indexOf(req.query.q) != -1 ||
-      excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
-      category.toLowerCase().indexOf(req.query.q) != -1
-  )
+//   const results = posts.filter(
+//     ({ frontmatter: { title, excerpt, category } }) =>
+//       title.toLowerCase().indexOf(req.query.q) != -1 ||
+//       excerpt.toLowerCase().indexOf(req.query.q) != -1 ||
+//       category.toLowerCase().indexOf(req.query.q) != -1
+//   )
 
-  console.log(results)
+//   console.log(results)
 
-  res.status(200).json(results)
-  // JSON.stringify({ results })
-}
+//   res.status(200).json(results)
+//   // JSON.stringify({ results })
+// }
