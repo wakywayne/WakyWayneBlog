ed292c66afc6251a6a01a19445b4545c794b5648
wakywayne <wakywayne80@gmail.com>
2022-05-19 14:39:21 -0400

    trying to fix deployment

--- components/Post.jsx
+++ components/Post.jsx
@@ -10,7 +10,7 @@ export default function Post({ post }) {
     return (
         <div className=' w-full px-10 py-6 bg-white rounded-lg shadow-md mt-6'>
             <Image
-                src={`/../public${post.frontmatter.cover_image}`}
+                src={`${post.frontmatter.cover_image}`}
                 alt={post.frontmatter.title}
                 height={420}
                 width={600}
